cb5f0bc7866f5a8674b8404843b68e66d1b59a13
Adan <adan@latailabs.com>
2023-02-25 11:04:43 -0600

    Django clase 1

---
+++ miproyecto/polls/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Pregunta(models.Model):
+    texto_pregunta = models.CharField(max_length=200)
+    fecha_pub = models.DateTimeField('Fecha de publicación')
+
+    def __str__(self):
+        return self.texto_pregunta
